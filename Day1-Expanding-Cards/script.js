996ea305b80cad96603ab6a75ed8a4bd849d29cf
Ajay Pradhan <pradhanajay14@gmail.com>
2021-06-16 03:39:56 +0530

    day1 expanding card completed

---
+++ Day1-Expanding-Cards/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach(function (panel) {
+  panel.addEventListener("click", function () {
+    removeActiveclass();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveclass() {
+  panels.forEach(function (panel) {
+    panel.classList.remove("active");
+  });
+}
