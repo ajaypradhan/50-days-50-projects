7e836264597432617706c69339ec9a6dd049bea0
Ajay Pradhan <pradhanajay14@gmail.com>
2021-06-20 00:02:38 +0530

    day4 hidden search done

--- Day4-Hidden-search/script.js
+++ Day4-Hidden-search/script.js
@@ -2,7 +2,7 @@ let search = document.querySelector(".search");
 let btn = document.querySelector(".btn");
 let input = document.querySelector(".input");
 
-btn.addEventListener('click' ,() =>{
-    search.classList.toggle('active');
-    input.focus();
-})
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
