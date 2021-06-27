19720387a4d2f5dc3e2d5b2c53bc776ac2288e28
Ajay Pradhan <pradhanajay14@gmail.com>
2021-06-28 00:57:04 +0530

    day10 dad joke done

---
+++ Day10-Dad-Jokes/script.js
@@ -0,0 +1,34 @@
+const jokeEL = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+generateJoke();
+
+
+//async await
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    };
+    const res = await fetch('https://icanhazdadjoke.com', config);
+    const data = await res.json();
+    jokeEL.innerHTML = data.joke;
+}
+
+
+
+//.then 
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json',
+//         },
+//     };
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then((res) => res.json())
+//         .then((data) => {
+//             jokeEL.innerHTML = data.joke;
+//         });
+// }
